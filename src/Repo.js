223ceb1d38550bb7b0e7a075e566c2bdb1aa98b7
Hugo Vieilledent <hugo.vieilledent@gmail.com>
2017-05-05 00:44:35 +0200

    clean gitHub suffix

--- src/Repo.js
+++ src/Repo.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default function Repo(props) {
-	let { repo } = props;
-	return (
-		<div className="Repo Nav">
-			<a href={ repo.html_url }> {repo.name}</a>
-			<a href={ repo.issues_url }>issues ({ repo.open_issues })</a>
-			<a href={ repo.pulls_url }>PR</a>
-		</div>
-	)
+class Repo extends Component {
+
+	clean (url) {
+		let filter = /{\/*.*}$/gm
+		return url.replace(filter, '');
+	}
+
+	render() {
+		let { repo } = this.props;
+		return (
+			<div className="Repo Nav">
+				<a href={ repo.html_url }> {repo.name}</a>
+				<a href={ this.clean(repo.issues_url) }>issues ({ repo.open_issues })</a>
+				<a href={ this.clean(repo.pulls_url) }>PR</a>
+			</div>
+		)
+	}
+
 }
+
+export default Repo;
