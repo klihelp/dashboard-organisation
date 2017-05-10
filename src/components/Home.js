b34704327e9ee6eea705cb73901616a05a1acb99
Hugo Vieilledent <hugo.vieilledent@gmail.com>
2017-05-10 17:50:01 +0200

    add events

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ export default function Home(props) {
 	let { model } = props;
 	return (
 		<nav className="Nav">
-			<a href={ model.blog } ref={ a => a && a.focus() }>{ model.login }</a>
+			<a href={ model.blog }>{ model.login }</a>
 			<a href={ model.html_url }>github</a>
 			<a href={ model.repos_url }>api.repos</a>
 			<a href={ model.events_url }>api.events</a>
