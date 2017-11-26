da5e68fdc2f814fc3890f1548a865b2a0eb9edbf
Hugo Vieilledent <hugo.vieilledent@gmail.com>
2017-11-26 10:27:03 +0100

    better info page

--- src/components/Infos.js
+++ src/components/Infos.js
@@ -2,13 +2,24 @@ import React from 'react';
 
 export default function Infos(props) {
 	let { model } = props;
+	console.log('model', model)
 	return (
-		<nav className="Nav">
-			<a href={ model.blog }>{ model.login }</a>
-			<a href={ model.html_url }>github</a>
-			<a href={ model.repos_url }>api.repos</a>
-			<a href={ model.events_url }>api.events</a>
-			<a href="https://github.com/internet4000/dashboard-organisation">github/dashboard-organisation</a>
-		</nav>
+		<ul className="Nav">
+			<li>
+				Login: <a href={ model.blog }>{ model.login }</a>
+			</li>
+			<li>
+				Website URL: <a href={ model.html_url }>github</a>
+			</li>
+			<li>
+				Github API repo URL:<a href={ model.repos_url }>api.repos</a>
+			</li>
+			<li>
+				Github API events URL: <a href={ model.events_url }>api.events</a>
+			</li>
+			<li>
+				Code: <a href="https://github.com/internet4000/dashboard-organisation">github/dashboard-organisation</a>
+			</li>
+		</ul>
 	)
 }
