62b23aee7cf68acd46db098f128f56d05097c3a3
Hugo Vieilledent <hugo.vieilledent@gmail.com>
2017-11-28 23:41:50 +0100

    add search on repos

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -1,17 +1,63 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Jets from 'jets';
 import Loading from './Loading';
 import Repo from './Repo';
 import withScrollTop from './withScrollTop';
 
-function Repos(props) {
-	if (!props.repos) {
-		return <Loading message="Loading list..."/>
+class Repos extends Component {
+
+	constructor() {
+		super();
+
+		this.state = {
+			jetsSearch: ''
+		}
 	}
-	return (
-		<div className="List List--repos">
-			{ props.repos.map((repo, i) => <Repo key={i} repo={repo} />) }
-		</div>
-	)
+
+	componentDidMount() {
+		const jets = new Jets({
+			callSearchManually: true,
+			contentTag: '#jetsContent'
+		});
+
+		this.state = {
+			jets
+		}
+	}
+
+	handleSearch = (e) => {
+		this.setState({
+	    [e.target.name] : e.target.value
+		});
+		this.state.jets.search(e.target.value)
+		console.log('this.state.jets', this.state.jets)
+	}
+
+	render() {
+		const { repos } = this.props;
+		const { jetsSearch } = this.state;
+
+		if (!repos) {
+			return <Loading message="Loading list..."/>
+		}
+
+		return (
+			<div>
+
+				<input
+				type="search"
+				name="jetsSearch"
+				id="jetsSearch"
+				placeholder="Filter"
+				onChange={ this.handleSearch }
+				value={ jetsSearch } />
+
+				<div id="jetsContent" className="List List--repos">
+					{ repos.map((repo, i) => <Repo key={i} repo={repo} />) }
+				</div>
+			</div>
+		)
+	}
 }
 
 export default withScrollTop(Repos);
